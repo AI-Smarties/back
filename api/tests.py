0f063b9b88b38f53cc8b3a62eba322a626faa6ae
vainiovesa <vesa.vainio@helsinki.fi>
2026-01-27 18:10:33 +0200

    Add message url name

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase  # pylint: disable=unused-import
 
+from django.urls import reverse # pylint: disable=unused-import
+
 # Create your tests here.
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import message_view
 
 urlpatterns = [
-    path("message/", message_view),
+    path("message/", message_view, name="message"),
 ]
