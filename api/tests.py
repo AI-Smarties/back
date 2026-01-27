57908d5f46b0939c773060ae05b0cb63d7fd3db7
vainiovesa <vesa.vainio@helsinki.fi>
2026-01-27 18:14:03 +0200

    Add tests skeleton

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,16 @@
-from django.test import TestCase  # pylint: disable=unused-import
+from django.test import TestCase, Client
 
-from django.urls import reverse # pylint: disable=unused-import
+from django.urls import reverse
 
 # Create your tests here.
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.message_url = reverse("message")
+        self.client = Client()
+
+    def test_message_GET(self):
+        pass
+
+    def test_message_POST(self):
+        pass
