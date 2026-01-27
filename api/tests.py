8e9ae223f435d3d255f5d290f7d8a1098149cd3a
vainiovesa <vesa.vainio@helsinki.fi>
2026-01-27 18:26:42 +0200

    Add tests

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 
 from django.urls import reverse
 
+import json
+
 # Create your tests here.
 
 class TestViews(TestCase):
@@ -10,7 +12,22 @@ class TestViews(TestCase):
         self.client = Client()
 
     def test_message_GET(self):
-        pass
+        response = self.client.get(self.message_url)
+
+        self.assertEqual(response.status_code, 405)
 
     def test_message_POST(self):
-        pass
+        data = {"text": "Test"}
+        response = self.client.post(
+            self.message_url, json.dumps(data), content_type="json"
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_message_POST_not_json(self):
+        data = {"text": "Test"}
+        response = self.client.post(
+            self.message_url, data
+        )
+
+        self.assertEqual(response.status_code, 400)
