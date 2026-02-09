fd3da86557b876a7af22c377d75e2a671be1055f
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-02-09 16:11:24 +0200

    fix: refactor

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import json
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from asr import StreamingASR
 
+
 app = FastAPI()
 
 
@@ -9,13 +10,13 @@ app = FastAPI()
 @app.websocket("/ws/audio/")
 async def audio_ws(ws: WebSocket):
     await ws.accept()
-    await ws.send_text(json.dumps({"type": "ready"})) # aloitus signaali
+    await ws.send_text(json.dumps({"type": "ready"})) # lähetetään vahvistus valmiudesta
 
     asr = None
 
     try:
         while True:
-            msg = await ws.receive() # vastaanottaa audion tai ohjaus signaalin
+            msg = await ws.receive() # vastaanottaa audiota tai ohjaussignaalin
 
             if "bytes" in msg and msg["bytes"]: # audio tulee binäärinä
                 if asr is None:
@@ -23,15 +24,16 @@ async def audio_ws(ws: WebSocket):
                 asr.push_audio(msg["bytes"]) # pushataan audio
                 continue
 
-            if "text" in msg and msg["text"]: # ohjaus signaali
+            if "text" in msg and msg["text"]: # ohjaussignaali
                 data = json.loads(msg["text"])
                 if data.get("action") == "stop": # lopetetaan
                     if asr:
-                        asr.stop() # lopetetaan ASR
+                        asr.stop()
                     continue
 
     except WebSocketDisconnect:
         pass
+
     finally:
         if asr:
-            asr.stop() # lopetetaan ASR
+            asr.stop()
