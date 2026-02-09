068fb0e9a3508d96420c4a40e594fbb5ec3f5ab9
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-02-09 18:32:21 +0200

    feat: initialize ASR when start control message arrives

--- src/main.py
+++ src/main.py
@@ -21,15 +21,13 @@ async def audio_ws(ws: WebSocket):
 
         elif msg["type"] == "websocket.receive":
             if "bytes" in msg:  # audio tulee binäärinä
-                if asr is None:
-                    asr = StreamingASR(ws)  # alustetaan ASR
                 asr.push_audio(msg["bytes"])
-                continue
 
-            if "text" in msg:  # kaikki muu kuin audio tulee tekstinä
+            elif "text" in msg:  # kaikki muu kuin audio tulee tekstinä
                 payload = json.loads(msg["text"])
                 if payload["type"] == "control":  # ohjaussignaali
-                    if payload["cmd"] == "stop":  # lopetetaan
-                        if asr:
-                            asr.stop()
-                        continue
+                    if payload["cmd"] == "start":  # aloitetaan
+                        asr = StreamingASR(ws)  # alustetaan ASR
+                    elif payload["cmd"] == "stop":  # lopetetaan
+                        asr.stop()
+                        asr = None
