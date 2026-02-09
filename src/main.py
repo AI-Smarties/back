ace4540cda04658b0c386b06d04ad6d12a8418f6
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-02-09 18:20:43 +0200

    fix: refactor code

--- src/main.py
+++ src/main.py
@@ -10,28 +10,27 @@ app = FastAPI()
 @app.websocket("/ws/audio/")
 async def audio_ws(ws: WebSocket):
     await ws.accept()
-    await ws.send_text(json.dumps({"type": "control", "cmd": "ready"})) # valmis vastaanottamaan
+    await ws.send_text(json.dumps({"type": "control", "cmd": "ready"}))  # valmis vastaanottamaan
 
     asr = None
 
     while True:
-        msg = await ws.receive() # vastaanottaa audiota tai ohjaussignaalin
+        msg = await ws.receive()
 
         if msg["type"] == "websocket.disconnect":
             break
 
         elif msg["type"] == "websocket.receive":
-
-            if "bytes" in msg: # audio tulee binäärinä
+            if "bytes" in msg:  # audio tulee binäärinä
                 if asr is None:
-                    asr = StreamingASR(ws) # alustetaan ASR
-                asr.push_audio(msg["bytes"]) # pushataan audio
+                    asr = StreamingASR(ws)  # alustetaan ASR
+                asr.push_audio(msg["bytes"])
                 continue
 
-            if "text" in msg:
+            if "text" in msg:  # kaikki muu kuin audio tulee tekstinä
                 payload = json.loads(msg["text"])
-                if payload["type"] == "control": # ohjaussignaali
-                    if payload["cmd"] == "stop": # lopetetaan
+                if payload["type"] == "control":  # ohjaussignaali
+                    if payload["cmd"] == "stop":  # lopetetaan
                         if asr:
                             asr.stop()
                         continue
