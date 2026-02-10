5d62101deb6e9f427c4af8d3aa60f0a5867202a4
Veeti Martinmäki <181813689+veetimar@users.noreply.github.com>
2026-02-10 15:25:56 +0200

    feat: tests and error handling for main.py

--- src/main.py
+++ src/main.py
@@ -24,22 +24,44 @@ async def audio_ws(ws: WebSocket):
             if msg["type"] == "websocket.receive":
                 if "bytes" in msg:  # audio tulee binäärinä
                     asr.push_audio(msg["bytes"])
-
                 elif "text" in msg:  # kaikki muu kuin audio tulee tekstinä
                     try:
                         payload = json.loads(msg["text"])
                     except json.JSONDecodeError:
                         await ws.send_json({"type": "error", "message": "Invalid JSON"})
-                        print("Invalid JSON", msg["text"])
+                        print(f"Invalid JSON: {msg['text']}")
+                        continue
+                    if "type" not in payload:
+                        await ws.send_json({"type": "error", "message": "Missing type in message"})
+                        print(f"Missing type in message: {payload}")
                         continue
                     if payload["type"] == "control":
+                        if "cmd" not in payload:
+                            await ws.send_json(
+                                {"type": "error", "message": "Missing command in control message"}
+                            )
+                            print(f"Missing command in control message: {payload}")
+                            continue
                         if payload["cmd"] == "start":
+                            if asr:
+                                asr.stop()
                             asr = StreamingASR(ws)
                         elif payload["cmd"] == "stop":
-                            asr.stop()
+                            if asr:
+                                asr.stop()
                             asr = None
+                        else:
+                            await ws.send_json({"type": "error", "message": "Unknown command"})
+                            print(f"Unknown command: {payload['cmd']}")
+                            continue
+                    else:
+                        await ws.send_json({"type": "error", "message": "Unknown message type"})
+                        print(f"Unknown message type: {payload['type']}")
+                        continue
+
     except WebSocketDisconnect:
         print("WebSocket disconnected")
+
     finally:
         if asr:
             asr.stop()
