592aa9259bcaba081c26eebb85906e4ac069f1b7
vainiovesa <vesa.vainio@helsinki.fi>
2026-02-10 17:14:29 +0200

    fix: import and url in db.py

--- src/db.py
+++ src/db.py
@@ -1,7 +1,6 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
@@ -9,14 +8,8 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "ai_smarties_db")
 
-DB_URL = f"""
-    postgresql://
-    {POSTGRES_USER}:
-    {POSTGRES_PASSWORD}@
-    {POSTGRES_HOST}:
-    {POSTGRES_PORT}/
-    {POSTGRES_DB}
-"""
+DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+DB_URL += f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 engine = create_engine(DB_URL)
 
