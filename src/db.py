f938e295c8f75a0cfebf076026a85864651b338f
vainiovesa <vesa.vainio@helsinki.fi>
2026-02-10 15:52:35 +0200

    feat: update db.py

--- src/db.py
+++ src/db.py
@@ -1,8 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DB_URL = "postgresql://postgres:postgres@localhost:5432/ai_smarties_db"
+POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "ai_smarties_db")
+
+DB_URL = f"""
+    postgresql://
+    {POSTGRES_USER}:
+    {POSTGRES_PASSWORD}@
+    {POSTGRES_HOST}:
+    {POSTGRES_PORT}/
+    {POSTGRES_DB}
+"""
 
 engine = create_engine(DB_URL)
 
