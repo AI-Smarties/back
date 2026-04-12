8f5ebd1bfca4bcab99eab85b9a4cd04cc0f55452
Sami Horttanainen <sami1horttanainen@gmail.com>
2026-04-12 15:42:32 +0300

    fix: pylint fix

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from os import environ
 from fastapi import FastAPI, WebSocket, HTTPException, Depends
 from sqlalchemy.exc import IntegrityError, NoResultFound
 import google
+from dotenv import load_dotenv
 
 from asr import StreamingASR
 from memory_extractor import extract_and_save_information_to_database
@@ -16,7 +17,6 @@ from context_service import build_context
 from gemini_live import GeminiLiveSession
 from auth import get_current_user, verify_token
 
-from dotenv import load_dotenv
 load_dotenv()
 
 @asynccontextmanager
